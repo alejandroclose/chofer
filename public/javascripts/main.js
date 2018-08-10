360dffdff6b7afc0bc3c1b3891bff246ee8301ca
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-08-10 20:21:42 +0200

    added routes using mapbox API

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -7,3 +7,36 @@ const map = new mapboxgl.Map({
   center: [2.15899, 41.38879],
   zoom: 14 // starting zoom
 });
+
+//Route
+map.on('load', function() {
+  getRoute();
+});
+
+function getRoute() {
+  console.log('routes started')
+  var start = [2.15899, 41.38879];
+  var end = [2.18606, 41.36890];
+  var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?geometries=geojson&access_token=' + mapboxgl.accessToken;
+  $.ajax({
+    method: 'GET',
+    url: directionsRequest,
+  }).done(function(data) {
+    var route = data.routes[0].geometry;
+    map.addLayer({
+      id: 'route',
+      type: 'line',
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'Feature',
+          geometry: route
+        }
+      },
+      paint: {
+        'line-width': 2
+      }
+    });
+    // this is where the code from the next step will go
+  });
+}
