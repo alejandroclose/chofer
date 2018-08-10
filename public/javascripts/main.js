a6b8599518fd464fb23f8f333da2c9a28e5f31fa
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-08-10 21:27:36 +0200

    added instructions using mapbox API

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,42 +1,88 @@
-console.log('main.js');
+console.log("main.js");
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWxlamFuZHJvY2xvc2UiLCJhIjoiY2prYTFkODU1MDhidzN2cWp1bmFseGxoZSJ9.HeAugAVJ8wr3NHFCOnkn-Q';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWxlamFuZHJvY2xvc2UiLCJhIjoiY2prYTFkODU1MDhidzN2cWp1bmFseGxoZSJ9.HeAugAVJ8wr3NHFCOnkn-Q";
 const map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
+  container: "map", // container id
+  style: "mapbox://styles/mapbox/streets-v9", // stylesheet location
   center: [2.15899, 41.38879],
   zoom: 14 // starting zoom
 });
 
 //Route
-map.on('load', function() {
+map.on("load", () => {
   getRoute();
 });
 
 function getRoute() {
-  console.log('routes started')
+  console.log("routes started");
   var start = [2.15899, 41.38879];
-  var end = [2.18606, 41.36890];
-  var directionsRequest = 'https://api.mapbox.com/directions/v5/mapbox/cycling/' + start[0] + ',' + start[1] + ';' + end[0] + ',' + end[1] + '?geometries=geojson&access_token=' + mapboxgl.accessToken;
+  var end = [2.18606, 41.3689];
+  var directionsRequest =
+    "https://api.mapbox.com/directions/v5/mapbox/cycling/" +
+    start[0] +
+    "," +
+    start[1] +
+    ";" +
+    end[0] +
+    "," +
+    end[1] +
+    "?steps=true&geometries=geojson&access_token=" +
+    mapboxgl.accessToken;
   $.ajax({
-    method: 'GET',
-    url: directionsRequest,
+    method: "GET",
+    url: directionsRequest
   }).done(function(data) {
     var route = data.routes[0].geometry;
+    var instructions = document.getElementById("instructions");
+    var steps = data.routes[0].legs[0].steps;
+    steps.forEach(function(step) {
+      instructions.insertAdjacentHTML(
+        "beforeend",
+        "<p>" + step.maneuver.instruction + "</p>"
+      );
+    });
     map.addLayer({
-      id: 'route',
-      type: 'line',
+      id: "route",
+      type: "line",
       source: {
-        type: 'geojson',
+        type: "geojson",
         data: {
-          type: 'Feature',
+          type: "Feature",
           geometry: route
         }
       },
       paint: {
-        'line-width': 2
+        "line-width": 3
       }
     });
-    // this is where the code from the next step will go
+    map.addLayer({
+      id: "start",
+      type: "circle",
+      source: {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: start
+          }
+        }
+      }
+    });
+    map.addLayer({
+      id: "end",
+      type: "circle",
+      source: {
+        type: "geojson",
+        data: {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: end
+          }
+        }
+      }
+    });
   });
 }
