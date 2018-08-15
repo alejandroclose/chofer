05ce4604d5576bde1e832459a2094542d0b4f922
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-08-15 20:33:12 +0200

    added destination to route

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -21,6 +21,7 @@ mapboxgl.accessToken =
       accessToken: mapboxgl.accessToken
     })
     document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+    const startLatLong = [position.coords.longitude, position.coords.latitude]; 
 
     geocoder.on('result', (data)=>{
       const endLatLong = data.result.center;
@@ -28,28 +29,15 @@ mapboxgl.accessToken =
       var destinationMarker = new mapboxgl.Marker()
     .setLngLat(endLatLong)
     .addTo(map);
+    getRoute();
 
-    });
-
-    
-
-    // var destinationMarker = new mapboxgl.Marker()
-    // .setLngLat(endLatLong)
-    // .addTo(map);
-
-
-
-const startLatLong = [position.coords.longitude, position.coords.latitude]; 
-
-//Route
-map.on("load", () => {
-  getRoute();
-});
+    //Route
+  
 
 function getRoute() {
   console.log("routes started");
   var start = startLatLong;
-  var end = [2.18606, 41.3689];
+  var end = endLatLong;
   var directionsRequest =
     "https://api.mapbox.com/directions/v5/mapbox/driving-traffic/" +
     start[0] +
@@ -118,6 +106,15 @@ function getRoute() {
     });
   });
 }
+
+    });
+
+
+
+
+
+
+
 }, (error) => {
   console.error('No puedo obtener la locaclización', error);
 })
